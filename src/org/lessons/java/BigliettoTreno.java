3878f47397b25ce76d650033353ccdd134ddfed9
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-05-31 15:37:34 +0200

    esercizio treno finito no bonus per ora

--- src/org/lessons/java/BigliettoTreno.java
+++ src/org/lessons/java/BigliettoTreno.java
@@ -1,2 +1,63 @@
-package org.lessons.java;public class BigliettoTreno {
+//Esercizio 1: Calcolo biglietto del treno
+//        Consegna: Il programma dovrà chiedere all’utente il numero (intero) di chilometri che vuole percorrere
+//        e l’età del passeggero (numero intero). Sulla base di queste informazioni dovrà calcolare il prezzo
+//        totale del viaggio, secondo queste regole:
+//        il prezzo del biglietto è definito in base ai km (0.21 € al km)
+//        va applicato uno sconto del 20% per i minorenni
+//        va applicato uno sconto del 40% per gli over 65
+//        Per chiedere informazioni all’utente sarà necessario usare la classe java.util.Scanner, come visto in
+//        classe stamattina.
+//        Bonus: formattare il risultato con un numero limitato di decimali utilizzando la classe DecimalFormat
+
+        package org.lessons.java;
+        import java.util.Scanner;
+
+public class BigliettoTreno {
+        public static void main(String[] args) {
+
+//               stabilisco le variabili di prezzo/km e scontistiche
+                double prezzoAlChilometro = 0.21;
+                double scontoSenior = 0.60;
+                double scontoJunior = 0.80;
+
+                //chiedo eta' e chilometri da percorrere dopo aver importato scanner
+
+                Scanner Scanner = new Scanner(System.in);
+
+                System.out.println("Quanti chilometri devi percorrere?");
+                int kmPasseggero = Scanner.nextInt();
+
+                System.out.println("Quanti anni hai?");
+                int anniPasseggero = Scanner.nextInt();
+
+//                se ho piu di 65 applico al prezzo base lo sconto e idem se ne ho meno di 18
+
+                double prezzo = kmPasseggero * prezzoAlChilometro;
+                if (anniPasseggero > 65) {
+                        prezzo = prezzo * scontoSenior;
+                } else if(anniPasseggero < 18) {
+                        prezzo = prezzo * scontoJunior;
+                }
+
+
+                System.out.println("Il prezzo sara' di " + prezzo + "€ per la tua  tratta " );
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
 }
