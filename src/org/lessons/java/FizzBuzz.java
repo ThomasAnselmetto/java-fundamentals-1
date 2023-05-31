10a6021c58633d8e9c9f80e67777211b2b823004
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-05-31 16:02:14 +0200

    fizzbuzz senza bonus

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -1,9 +1,31 @@
 //Esercizio 2: FizzBuzz
-//        Consegna: Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+//        Consegna: Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz”
+//        al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di
+//        5 stampi “FizzBuzz”.
 //        Esempio: 1 2 Fizz 4 Buzz Fizz 7 8 Fizz……
-//        Bonus: invece che 100, chiedere all’utente quanti numeri vuole stampare (il valore inserito dall’utente deve essere maggiore di 0 e minore di 1000)
+//        Bonus: invece che 100, chiedere all’utente quanti numeri vuole stampare (il valore
+//        inserito dall’utente deve essere maggiore di 0 e minore di 1000)
+
 package org.lessons.java;
-
+import java.util.Scanner;
 public class FizzBuzz {
+    public static void main(String[] args) {
 
+//        chiedo un numero di caselle all'utente
+
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("quante caselle vuoi?");
+        int numeroCaselleUtente = Scanner.nextInt();
+
+//    ciclo mettendo come limite il numero scelto dall'utente' creo una serie di condizioni per le quali se i resto numero(3-5-multiplo di entrambi) == 0 stampo cio' che e' richiesto dalla traccia
+
+        for (int i = 1; i < numeroCaselleUtente; i++) {
+            if (i % 5 == 0 && i % 3 == 0) System.out.println("FizzBuzz");
+            else if (i % 5 == 0) System.out.println("Buzz");
+            else if(i % 3 == 0) System.out.println("Fizz");
+            else System.out.println(i);
+        }
+
+
+    }
 }
